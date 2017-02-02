bc079ddf648d6d5aeaf0d73e42639a8621c6d6a7
Upkar Lidder <lidderupk@gmail.com>
2017-02-01 16:01:41 -0800

    set up gulp and bower

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodejs-express40-ps
+
+## Versioning (x.y.z - maj.min.point)
+1. "^" - install same major version (automatically update minor updates)
+2. "~" - install largest minor versions on update (do not update major)
+3. nothing - use this version only
+
+## Notes
+- --save vs --save-dev
+
+
--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-var express = require('express');
+/* jshint node: true */
+/* global require */
+'use strict';
 
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function (req, res) {
+    res.send('hello world \n');
+});
+
+app.get('/books', function (req, res) {
+    res.send('hello books \n');
+});
+
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
+});
